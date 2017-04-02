8c582f78a582a4a9f7113103bebc0825fda8a70a
jarra <suhrawardi@gmail.com>
2017-04-02 18:58:42 +0200

    Moved WordCounter to its own module

--- Main.hs
+++ Main.hs
@@ -5,37 +5,7 @@
 
 module Main where
 
-import Data.Char (toLower)
-import Data.List (sort, sortBy)
-
-type Text = String
-
-count = 10
-
-lineCounter :: Text -> String
-lineCounter text = show (length (lines text))
-
-wordCounter :: Text -> String
-wordCounter text = show (length (words text))
-
-showRun :: (Int, String) -> String
-showRun (n, w) = w ++ ": " ++ show n ++ "\n"
-
-countRuns :: [String] -> [(Int, String)]
-countRuns []     = []
-countRuns (w:ws) = (1 + length us, w):countRuns vs
-                   where (us, vs) = span (==w) ws
-
-sortRuns :: [(Int, String)] -> [(Int, String)]
-sortRuns = sortBy (flip compare)
-
-commonWords :: Int -> Text -> String
-commonWords n = concatMap showRun . take n .
-                sortRuns . countRuns . sort .
-                words . map toLower
+import WordCounter
 
 main :: IO ()
-main = do content <- getContents
-          putStrLn $ lineCounter content ++ " lines"
-          putStrLn $ wordCounter content ++ " words"
-          putStrLn $ commonWords count content
+main = countWords
